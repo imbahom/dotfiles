92da35f1cb29fd38e51262e11e9bd1e8d6dd5f33
imbahom <imbahom@gmail.com>
2014-06-18 16:59:45 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env sh
 
+git clone https://github.com/gmarik/vundle.git ~/.dotfiles/vim/bundle/vundle
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
+chsh -s /bin/zsh
 
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim ~/.vim
+
+ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+
+echo "Please BundleInstall~~~~~~~~~~~~~~~~~"
