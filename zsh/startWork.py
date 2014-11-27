cbeebedae83e424784fe6c78a65fb6240569533b
imbahom <imbahom@gmail.com>
2014-11-27 11:42:41 +0800

    start new tmux session with a cmd

--- zsh/startWork.py
+++ zsh/startWork.py
@@ -8,5 +8,5 @@ cmd = "tmux attach -t " + sessionName
 re = os.popen(cmd).read()
 print(re)
 if string.find(re,"no sessions") == -1:
-    cmd = "tmux new-session -d -s " + sessionName + " && tmux send -t work 'cd ~/Documents/code/Project2/trunk/Developer/client/scripts && vim **/*.lua' ENTER && tmux attach -t " + sessionName
+    cmd = "tmux new-session -d -s " + sessionName + " && tmux send -t work " + sys.argv[1] + " ENTER && tmux attach -t " + sessionName
     os.system(cmd)
